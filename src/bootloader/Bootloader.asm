a7ec24a53c4375492f6691a520258c691a238f46
Medy-Qwerty <73920660+Medy-Qwerty@users.noreply.github.com>
2023-09-11 22:24:47 +0600

    Added Simple "Trid-Kernel"

--- src/bootloader/Bootloader.asm
+++ src/bootloader/Bootloader.asm
@@ -1,7 +1,36 @@
 mov ah, 0x0e
 
 mov al, 'T'
+int 0x10
 
+mov al, 'r'
+int 0x10
+
+mov al, 'i'
+int 0x10
+
+mov al, 'd'
+int 0x10
+
+mov al, '-'
+int 0x10
+
+mov al, 'K'
+int 0x10
+
+mov al, 'e'
+int 0x10
+
+mov al, 'r'
+int 0x10
+
+mov al, 'n'
+int 0x10
+
+mov al, 'e'
+int 0x10
+
+mov al, 'l'
 int 0x10
 
 jmp $
