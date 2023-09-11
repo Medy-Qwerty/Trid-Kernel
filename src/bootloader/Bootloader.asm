e4c75f338d57c3c2d53d403e25d699d4114f29eb
Medy-Qwerty <73920660+Medy-Qwerty@users.noreply.github.com>
2023-09-11 22:25:55 +0600

    Added Jump Labels (Function)

--- src/bootloader/Bootloader.asm
+++ src/bootloader/Bootloader.asm
@@ -1,40 +1,11 @@
-mov ah, 0x0e
 
-mov al, 'T'
-int 0x10
-
-mov al, 'r'
-int 0x10
-
-mov al, 'i'
-int 0x10
-
-mov al, 'd'
-int 0x10
-
-mov al, '-'
-int 0x10
-
-mov al, 'K'
-int 0x10
-
-mov al, 'e'
-int 0x10
-
-mov al, 'r'
-int 0x10
-
-mov al, 'n'
-int 0x10
-
-mov al, 'e'
-int 0x10
-
-mov al, 'l'
-int 0x10
+call PrintString
 
 jmp $
 
 times 510-($-$$) db 0
 
 dw 0xaa55
+
+PrintString:
+    ret
