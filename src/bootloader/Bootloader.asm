2cc966dc198e441392a5acdcb3669bdf52af576b
Medy-Qwerty <73920660+Medy-Qwerty@users.noreply.github.com>
2023-09-11 22:35:46 +0600

    Fix Bugs

--- src/bootloader/Bootloader.asm
+++ src/bootloader/Bootloader.asm
@@ -10,9 +10,15 @@ jmp $
 
 PrintFunction:
     mov ah, 0x0e
-    mov al, [bx]
-    int 0x10
-    ret
+    .Loop:
+        cmp [bx], byte 0
+        je .Exit
+        mov al, [bx]
+        int 0x10
+        inc bx
+        jmp .Loop
+    .Exit:
+        ret
 
 WelcomeString:
     db 'Trid-Kernel Alpha v0.0.2',0
