3b6ea83b93b4220356942b4145672bca40255c41
Medy-Qwerty <73920660+Medy-Qwerty@users.noreply.github.com>
2023-09-11 22:28:27 +0600

    Defining a String

--- src/bootloader/Bootloader.asm
+++ src/bootloader/Bootloader.asm
@@ -1,11 +1,14 @@
-
 call PrintString
 
 jmp $
 
-times 510-($-$$) db 0
-
-dw 0xaa55
 
 PrintString:
     ret
+
+WelcomeString:
+    db 'Trid-Kernel Alpha v0.0.2',0
+
+times 510-($-$$) db 0
+
+dw 0xaa55
