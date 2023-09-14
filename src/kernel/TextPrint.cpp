4bbe7a2bdcea8c12d359d5aca58aec5f001fa0ba
Medy-Qwerty <73920660+Medy-Qwerty@users.noreply.github.com>
2023-09-14 20:54:40 +0600

    Writing SetCursorPosition function

--- src/kernel/TextPrint.cpp
+++ src/kernel/TextPrint.cpp
@@ -1,6 +1,10 @@
 #pragma once
+#include "IO.cpp"
 #define VGA_MEMORY (unsigned char*)0xb8000
 
-void SetCursorPosition(unsigned char position) {
-    
+void SetCursorPosition(unsigned short position) {
+    outb(0x3D4, 0x0F);
+    outb(0x3D5, (unsigned char)(position & 0xFF));
+    outb(0x3D4, 0x0E);
+    outb(0x3D5, (unsigned char)((position >> 8) & 0xFF));
 }
