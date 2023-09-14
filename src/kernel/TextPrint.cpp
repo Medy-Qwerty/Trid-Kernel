1252018bea75e38159e7c2c6844eb11d349241d3
Medy-Qwerty <73920660+Medy-Qwerty@users.noreply.github.com>
2023-09-14 20:57:33 +0600

    Added CursorPosition Global Variable

--- src/kernel/TextPrint.cpp
+++ src/kernel/TextPrint.cpp
@@ -2,9 +2,14 @@
 #include "IO.cpp"
 #define VGA_MEMORY (unsigned char*)0xb8000
 
+unsigned short CursorPosition;
+
 void SetCursorPosition(unsigned short position) {
     outb(0x3D4, 0x0F);
     outb(0x3D5, (unsigned char)(position & 0xFF));
     outb(0x3D4, 0x0E);
     outb(0x3D5, (unsigned char)((position >> 8) & 0xFF));
+
+    CursorPosition = position;
+
 }
