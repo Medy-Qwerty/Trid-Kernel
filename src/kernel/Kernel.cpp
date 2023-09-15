dfec735d744d982d620ed18d2266bea1e6780189
Medy-Qwerty <73920660+Medy-Qwerty@users.noreply.github.com>
2023-09-15 09:35:39 +0600

    Displaying Hex Values

--- src/kernel/Kernel.cpp
+++ src/kernel/Kernel.cpp
@@ -1,6 +1,12 @@
 #include "TextPrint.cpp"
 extern "C" void _start() {
     SetCursorPosition(PositionFromCoords(0, 0));
-    PrintString("Welcome to Trid-Kernel v0.0.9\n\r");
+    PrintString("Welcome to Trid-Kernel v0.0.9\n\r\n\r");
+
+    PrintString("Hex to String Function Test: ");
+    PrintString(HexToString(0x1234abcd));
+    PrintString("\n\r");
+    PrintString("String to Hex Function Test: ");
+    PrintString(HexToString("Trid-Kernel"));
     return;
 }
