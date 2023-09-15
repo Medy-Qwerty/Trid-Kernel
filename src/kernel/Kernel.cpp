d755c20fd09770c01bc24f415876f769262f6bfe
Medy-Qwerty <73920660+Medy-Qwerty@users.noreply.github.com>
2023-09-15 09:27:37 +0600

    Added String Formatting

--- src/kernel/Kernel.cpp
+++ src/kernel/Kernel.cpp
@@ -1,6 +1,6 @@
 #include "TextPrint.cpp"
 extern "C" void _start() {
     SetCursorPosition(PositionFromCoords(0, 0));
-    PrintString("Welcome to Trid-Kernel v0.0.9");
+    PrintString("Welcome to Trid-Kernel v0.0.9\n\r");
     return;
 }
