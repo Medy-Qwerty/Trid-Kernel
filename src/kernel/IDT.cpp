459198073e37a6720a345800fadfe6f8e0ec3c8d
Medy-Qwerty <73920660+Medy-Qwerty@users.noreply.github.com>
2023-09-16 11:39:01 +0600

    Added IDT Descriptor

--- src/kernel/IDT.cpp
+++ src/kernel/IDT.cpp
@@ -14,5 +14,13 @@ extern IDT64 _idt[256];
 extern uint_64 isr1;
 
 void InitializeIDT() {
-
+    for (uint_64 t = 0; t < 256; t++) {
+        _idt[t].zero = 0;
+        _idt[t].offset_low = (uint_16)(((uint_64)&isr1 & 0x000000000000ffff));
+        _idt[t].offset_mid = (uint_16)(((uint_64)&isr1 & 0x00000000ffff0000) >> 16);
+        _idt[t].offset_high = (uint_32)(((uint_64)&isr1 & 0xffffffff00000000) >> 32);
+        _idt[t].ist = 0;
+        _idt[t].selector = 0x08;
+        _idt[t].types_attr = 0x8e;
+    }
 }
