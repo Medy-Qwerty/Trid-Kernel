eccf0f678c4c37436ca87e083e3167fe7556834e
Medy-Qwerty <73920660+Medy-Qwerty@users.noreply.github.com>
2023-09-17 12:05:22 +0600

    Added Left Shift

--- src/kernel/Kernel.cpp
+++ src/kernel/Kernel.cpp
@@ -4,9 +4,18 @@
 extern const char Test[];
 extern const char Logo[];
 
+bool LeftShiftPressed = false;
+
 void KeyboardHandler(uint_8 scanCode, uint_8 chr) {
     if (chr != 0) {
-        PrintChar(chr);
+        switch (LeftShiftPressed) {
+            case true:
+                PrintChar(chr - 32);
+                break;
+            case false:
+                PrintChar(chr);
+                break;
+        }
     }
     else {
         switch (scanCode) {
@@ -14,6 +23,12 @@ void KeyboardHandler(uint_8 scanCode, uint_8 chr) {
                 SetCursorPosition(CursorPosition - 1);
                 PrintChar(' ');
                 SetCursorPosition(CursorPosition - 1);
+                break;
+            case 0x2A: // Left Shift
+                LeftShiftPressed = true;
+                break;
+            case 0xAA: // Left Shift Released
+                LeftShiftPressed = false;
                 break;
         }
     }
@@ -24,7 +39,7 @@ extern "C" void _start() {
     SetCursorPosition(PositionFromCoords(0, 0));
     PrintString(Logo, BACKGROUND_BLINKINGRED | FOREGROUND_LIGHTCYAN);
     PrintString("\n\r");
-    PrintString("Welcome to Trid-Kernel Alpha v0.1.3\n\r\n\r", BACKGROUND_BLINKINGRED | FOREGROUND_LIGHTCYAN);
+    PrintString("Welcome to Trid-Kernel Alpha v0.1.4\n\r\n\r", BACKGROUND_BLINKINGRED | FOREGROUND_LIGHTCYAN);
 
     PrintString("Hex to String Function Test: ");
     PrintString(HexToString(0x1234abcd));
