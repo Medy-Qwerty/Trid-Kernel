8d8821789ae088e633972d86cbf5116adcf07fe5
Medy-Qwerty <73920660+Medy-Qwerty@users.noreply.github.com>
2023-09-17 11:26:06 +0600

    Added RemapPIC Function

--- src/kernel/IO.cpp
+++ src/kernel/IO.cpp
@@ -20,3 +20,22 @@ unsigned char inb(unsigned short port) {
     : "Nd"(port));
     return returnVal;
 }
+
+void RemapPic() {
+    uint_8 a1, a2;
+
+    a1 = inb(PIC1_DATA);
+    a2 = inb(PIC2_DATA);
+
+    outb(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);
+    outb(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
+    outb(PIC1_DATA, 0);
+    outb(PIC2_DATA, 8);
+    outb(PIC1_DATA, 4);
+    outb(PIC2_DATA, 2);
+    outb(PIC1_DATA, ICW4_8086);
+    outb(PIC2_DATA, ICW4_8086);
+
+    outb(PIC1_DATA, a1);
+    outb(PIC2_DATA, a2);
+}
