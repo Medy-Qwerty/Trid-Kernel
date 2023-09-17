22dda31db6ab6547428a354f752414a7ab9df6f7
Medy-Qwerty <73920660+Medy-Qwerty@users.noreply.github.com>
2023-09-17 11:35:11 +0600

    Filling the Table

--- src/kernel/IDT.cpp
+++ src/kernel/IDT.cpp
@@ -31,6 +31,7 @@ void InitializeIDT() {
 }
 
 extern "C" void isr1_handler() {
+    uint_8 scanCode = inb(0x60);
     PrintString(HexToString(inb(0x60)));
     outb(0x20, 0x20);
     outb(0xa0, 0x20);
