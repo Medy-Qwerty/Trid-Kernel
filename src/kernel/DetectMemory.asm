2c119cb05a34bd01f3b2b17ea6652f229f0cc9c4
Medy-Qwerty <73920660+Medy-Qwerty@users.noreply.github.com>
2023-09-19 10:10:35 +0600

    Added DetectMemory Function

--- src/kernel/DetectMemory.asm
+++ src/kernel/DetectMemory.asm
@@ -1,2 +1,25 @@
 MemoryReigonCount:
     db 0
+    GLOBAL MemoryReigonCount
+
+DetectMemory:
+    mov ax, 0
+    mov es, ax
+    mov di, 0x5000
+    mov edx, 0x534D4150
+    xor ebx, ebx
+
+    .repeat:
+        mov eax, 0xE820
+        mov ecx, 24
+        int 0x15
+
+        cmp ebx, 0
+        je .finished
+
+        add di, 24
+        inc byte [MemoryReigonCount]
+        jmp .repeat
+
+    .finished:
+        ret
