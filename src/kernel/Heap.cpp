d4539332649b683f2c184e42307d4f1974093c1e
Medy-Qwerty <junusiov.dias10@gmail.com>
2023-09-25 10:05:54 +0600

    Added MemorySegmentHeader

--- src/kernel/Heap.cpp
+++ src/kernel/Heap.cpp
@@ -1,1 +1,3 @@
 #include "Heap.h"
+
+MemorySegmentHeader* FirstFreeMemorySegment;
--- src/kernel/Heap.h
+++ src/kernel/Heap.h
@@ -1,1 +1,11 @@
 #pragma once
+#include "Typedefs.h"
+
+struct MemorySegmentHeader {
+    uint_64 MemoryLength;
+    MemorySegmentHeader* NextSegment;
+    MemorySegmentHeader* PreviousSegment;
+    MemorySegmentHeader* NextFreeSegment;
+    MemorySegmentHeader* PreviousFreeSegment;
+    bool Free;
+};
