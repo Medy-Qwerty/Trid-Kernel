0576a1a656dc222827859d0c14e5e90f5844d322
Medy-Qwerty <junusiov.dias10@gmail.com>
2023-09-27 10:25:27 +0600

    Added Free Function

--- src/kernel/Heap.cpp
+++ src/kernel/Heap.cpp
@@ -58,3 +58,25 @@ void* malloc(uint_64 size) {
     }
     return 0; // We Should Never Get There
 }
+
+void free(void* address) {
+    MemorySegmentHeader* currentMemorySegment = ((MemorySegmentHeader*)address) - 1;
+    currentMemorySegment->Free = true;
+
+    if (currentMemorySegment < FirstFreeMemorySegment) FirstFreeMemorySegment = currentMemorySegment;
+
+    if (currentMemorySegment->NextFreeSegment != 0) {
+        if (currentMemorySegment->NextFreeSegment->PreviousFreeSegment < currentMemorySegment)
+            currentMemorySegment->NextFreeSegment->PreviousFreeSegment = currentMemorySegment;
+    }
+    if (currentMemorySegment->PreviousFreeSegment != 0) {
+        if (currentMemorySegment->PreviousFreeSegment->NextFreeSegment < currentMemorySegment)
+            currentMemorySegment->PreviousFreeSegment->NextFreeSegment = currentMemorySegment;
+    }
+    if (currentMemorySegment->NextSegment != 0) {
+        currentMemorySegment->NextSegment->PreviousSegment = currentMemorySegment;
+    }
+     if (currentMemorySegment->PreviousSegment != 0) {
+        currentMemorySegment->PreviousSegment->NextSegment = currentMemorySegment;
+    }
+}
