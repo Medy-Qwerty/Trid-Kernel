5c584738959e4fb15bf24f632886f0d5125d46fc
Medy-Qwerty <junusiov.dias10@gmail.com>
2023-09-28 20:32:58 +0600

    Added Realloc

--- src/kernel/Heap.h
+++ src/kernel/Heap.h
@@ -15,6 +15,8 @@ void InitializeHeap(uint_64 heapAddress, uint_64 heapLength);
 
 void* calloc(uint_64 size);
 
+void* realloc(void* address, uint_64 newSize);
+
 void* malloc(uint_64 size);
 
 void free(void* address);
--- src/kernel/Kernel.cpp
+++ src/kernel/Kernel.cpp
@@ -24,7 +24,7 @@ extern "C" void _start() {
 
     PrintString(Logo, BACKGROUND_BLINKINGGREEN | FOREGROUND_LIGHTCYAN);
     PrintString("\n\r");
-    PrintString("Welcome to Trid-Kernel Beta v0.2.3\n\r\n\r", BACKGROUND_BLINKINGGREEN | FOREGROUND_LIGHTCYAN);
+    PrintString("Welcome to Trid-Kernel Beta v0.2.4\n\r\n\r", BACKGROUND_BLINKINGGREEN | FOREGROUND_LIGHTCYAN);
 
     PrintString("Hex to String Function Test: ");
     PrintString(HexToString(0x1234abcd), BACKGROUND_BLUE | FOREGROUND_RED);
