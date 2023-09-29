55dd9aea57ec7af84fa4f281171e2a85faa9e573
Medy-Qwerty <junusiov.dias10@gmail.com>
2023-09-29 09:54:19 +0600

    Testing

--- src/kernel/Kernel.cpp
+++ src/kernel/Kernel.cpp
@@ -13,10 +13,16 @@ extern "C" void _start() {
     MainKeyboardHandler = KeyboardHandler;
     InitializeIDT();
     InitializeHeap(0x100000, 0x100000);
+    malloc(0x4000);
+
+    float testFloat = 3.7f;
+    float testFloatNegative = -3.7f;
     void* TestMemoryAddress = malloc(0x10);
     void* TestMemoryAddress2 = malloc(0x10);
     void* TestMemoryAddress3 = malloc(0x10);
     void* TestMemoryAddress4 = malloc(0x60);
+    uint_64* TestMemoryAddress5 = (uint_64*)aligned_alloc(0x4000, 0x08);
+    MemoryMapEntry** usableMemoryMaps = GetUsableMemoryRegions();
     free(TestMemoryAddress);
     free(TestMemoryAddress2);
     free(TestMemoryAddress3);
@@ -24,7 +30,7 @@ extern "C" void _start() {
 
     PrintString(Logo, BACKGROUND_BLINKINGGREEN | FOREGROUND_LIGHTCYAN);
     PrintString("\n\r");
-    PrintString("Welcome to Trid-Kernel Beta v0.2.4\n\r\n\r", BACKGROUND_BLINKINGGREEN | FOREGROUND_LIGHTCYAN);
+    PrintString("Welcome to Trid-Kernel Beta v0.2.5\n\r\n\r", BACKGROUND_BLINKINGGREEN | FOREGROUND_LIGHTCYAN);
 
     PrintString("Hex to String Function Test: ");
     PrintString(HexToString(0x1234abcd), BACKGROUND_BLUE | FOREGROUND_RED);
@@ -38,8 +44,6 @@ extern "C" void _start() {
     PrintString(IntegerToString(-123456789), BACKGROUND_BLUE | FOREGROUND_RED);
 
     PrintString("\n\r");
-    float testFloat = 3.7f;
-    float testFloatNegative = -3.7f;
     PrintString("Float to String Function Test: ");
     PrintString(FloatToString(testFloat, 2), BACKGROUND_BLUE | FOREGROUND_RED);
     PrintString(" : ");
@@ -50,7 +54,6 @@ extern "C" void _start() {
     PrintString(IntegerToString(MemoryRegionCount), BACKGROUND_BLUE | FOREGROUND_RED);
     //PrintString("\n\r");
     //PrintString("Memory Regions: \n");
-    MemoryMapEntry** usableMemoryMaps = GetUsableMemoryRegions();
 
     //for (uint_8 i = 0; i < UsableMemoryRegionCount; i++) {
         //MemoryMapEntry* memMap = usableMemoryMaps[i];
@@ -58,9 +61,12 @@ extern "C" void _start() {
    //}
 
     PrintString("\n\r");
-    PrintString("Testing Dynamic Memory: ");
+    PrintString("Testing Dynamic Memory: ", BACKGROUND_BLUE | FOREGROUND_GREEN);
     PrintString("Malloc: ");
     PrintString(HexToString((uint_64)TestMemoryAddress), BACKGROUND_BLUE | FOREGROUND_RED);
+    PrintString(" ");
+    PrintString("Aligned_Alloc: ");
+    PrintString(HexToString((uint_64)TestMemoryAddress5), BACKGROUND_BLUE | FOREGROUND_RED);
 
     PrintString("\n\r");
     PrintString("IDT Test: ");
